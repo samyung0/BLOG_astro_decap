5ca2d64edb0181d780a2c0dca56a60f321c84cc9
sam <yungchinpang999@gmail.com>
2023-12-27 21:25:12 +0800

    skip astor check when building

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,2 @@
-/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-declare namespace App {
-  interface Locals {
-  }
-}
-
-interface ImportMetaEnv {
-  readonly STRAPI_URL: string;
-}
+/// <reference path="../.astro/types.d.ts" />
