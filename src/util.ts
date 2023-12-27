500b4ee564d6405680039bd8515bcd284e1cc541
sam <yungchinpang999@gmail.com>
2023-12-27 22:57:00 +0800

    fix

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
 import { getCollection } from "astro:content";
-export const posts = await getCollection("posts")
+export const posts = await getCollection("posts");
 export const authors = await getCollection("authors");
 export const tags = await getCollection("tags");
